60775c23a518038f3c734228a7ecbb73d124449e
Sumit-Tiwari830 <kiransumit2231@gmail.com>
2026-06-06 18:17:19 +0000

    complete backend implementation

--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -1,6 +1,28 @@
+const jwt = require("jsonwebtoken");
+
 const auth = (req, res, next) => {
-    console.log("Auth middleware called");
-    next();
+    try {
+        const token = req.headers.authorization;
+
+        if (!token) {
+            return res.status(401).json({
+                message: "Unauthorized"
+            });
+        }
+
+        const decoded = jwt.verify(
+            token,
+            process.env.JWT_SECRET
+        );
+
+        req.user = decoded;
+
+        next();
+    } catch (err) {
+        return res.status(401).json({
+            message: "Invalid token"
+        });
+    }
 };
 
 module.exports = auth;
