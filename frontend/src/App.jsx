e6f079d6f83f53baa700248087f4ae7e49d5927f
Sumit-Tiwari830 <kiransumit2231@gmail.com>
2026-06-07 17:39:52 +0000

    feat: Update App component to render ChooseUser and add SubjectForm and ViewSubject components for subject management
    Signed-off-by: Sumit-Tiwari830 <kiransumit2231@gmail.com>

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,10 @@
 import ChooseUser from "./pages/ChooseUser";
 import LoginPage from "./pages/LoginPage";
 import Logout from "./pages/Logout";
+;
+import Homepage from "./pages/Homepage";
 function App() {
-  return <Logout
+  return <ChooseUser
   />;
 }
 
