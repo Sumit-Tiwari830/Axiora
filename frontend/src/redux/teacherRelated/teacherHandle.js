8ce125fafad8a0d0a749c97a21bf5f6b8ad0813f
Sumit-Tiwari830 <kiransumit2231@gmail.com>
2026-06-07 21:01:53 +0000

    feat: Add teacher-related Redux slice and async actions for managing teachers and subjects
    Signed-off-by: Sumit-Tiwari830 <kiransumit2231@gmail.com>

---
+++ frontend/src/redux/teacherRelated/teacherHandle.js
@@ -0,0 +1,79 @@
+import axios from "axios";
+import {
+    getRequest,
+    getSuccess,
+    getFailed,
+    getError,
+    postDone,
+    doneSuccess,
+} from "./teacherSlice";
+
+const baseUrl = process.env.REACT_APP_BASE_URL;
+
+// ==================== GET ALL TEACHERS ====================
+
+export const getAllTeachers = (id) => async (dispatch) => {
+    dispatch(getRequest());
+
+    try {
+        const res = await axios.get(
+            `${baseUrl}/Teachers/${id}`
+        );
+
+        if (res.data?.message) {
+            dispatch(getFailed(res.data.message));
+            return;
+        }
+
+        dispatch(getSuccess(res.data));
+    } catch (err) {
+        dispatch(getError(err.response?.data || err.message));
+    }
+};
+
+// ==================== GET TEACHER DETAILS ====================
+
+export const getTeacherDetails = (id) => async (dispatch) => {
+    dispatch(getRequest());
+
+    try {
+        const res = await axios.get(
+            `${baseUrl}/Teacher/${id}`
+        );
+
+        dispatch(doneSuccess(res.data));
+    } catch (err) {
+        dispatch(getError(err.response?.data || err.message));
+    }
+};
+
+// ==================== ASSIGN SUBJECT ====================
+
+export const updateTeachSubject =
+    (teacherId, teachSubject) => async (dispatch) => {
+        dispatch(getRequest());
+
+        try {
+            const res = await axios.put(
+                `${baseUrl}/TeacherSubject`,
+                {
+                    teacherId,
+                    teachSubject,
+                },
+                {
+                    headers: {
+                        "Content-Type": "application/json",
+                    },
+                }
+            );
+
+            if (res.data?.message) {
+                dispatch(getFailed(res.data.message));
+                return;
+            }
+
+            dispatch(postDone());
+        } catch (err) {
+            dispatch(getError(err.response?.data || err.message));
+        }
+    };
