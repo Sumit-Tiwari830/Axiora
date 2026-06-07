08d2357d3a9b63d3689c332100b7fd997b04ffd3
Sumit-Tiwari830 <kiransumit2231@gmail.com>
2026-06-07 19:20:06 +0000

    feat: Add TeacherSideBar component for navigation in teacher interface
    Signed-off-by: Sumit-Tiwari830 <kiransumit2231@gmail.com>

---
+++ frontend/src/pages/teacher/TeacherSideBar.jsx
@@ -0,0 +1,145 @@
+import React from "react";
+import {
+    Divider,
+    ListItemButton,
+    ListItemIcon,
+    ListItemText,
+    ListSubheader,
+} from "@mui/material";
+
+import { Link, useLocation } from "react-router-dom";
+import { useSelector } from "react-redux";
+
+import HomeIcon from "@mui/icons-material/Home";
+import ExitToAppIcon from "@mui/icons-material/ExitToApp";
+import AccountCircleOutlinedIcon from "@mui/icons-material/AccountCircleOutlined";
+import AnnouncementOutlinedIcon from "@mui/icons-material/AnnouncementOutlined";
+import ClassOutlinedIcon from "@mui/icons-material/ClassOutlined";
+
+const TeacherSideBar = () => {
+    const location = useLocation();
+
+    const { currentUser } = useSelector(
+        (state) => state.user
+    );
+
+    const className =
+        currentUser?.teachSclass?.sclassName ||
+        "Not Assigned";
+
+    return (
+        <>
+            <React.Fragment>
+                <ListItemButton
+                    component={Link}
+                    to="/Teacher/dashboard"
+                >
+                    <ListItemIcon>
+                        <HomeIcon
+                            color={
+                                location.pathname === "/" ||
+                                    location.pathname ===
+                                    "/Teacher/dashboard"
+                                    ? "primary"
+                                    : "inherit"
+                            }
+                        />
+                    </ListItemIcon>
+
+                    <ListItemText primary="Home" />
+                </ListItemButton>
+
+                <ListItemButton
+                    component={Link}
+                    to="/Teacher/class"
+                >
+                    <ListItemIcon>
+                        <ClassOutlinedIcon
+                            color={
+                                location.pathname.startsWith(
+                                    "/Teacher/class"
+                                )
+                                    ? "primary"
+                                    : "inherit"
+                            }
+                        />
+                    </ListItemIcon>
+
+                    <ListItemText
+                        primary={`Class ${className}`}
+                    />
+                </ListItemButton>
+
+                <ListItemButton
+                    component={Link}
+                    to="/Teacher/complain"
+                >
+                    <ListItemIcon>
+                        <AnnouncementOutlinedIcon
+                            color={
+                                location.pathname.startsWith(
+                                    "/Teacher/complain"
+                                )
+                                    ? "primary"
+                                    : "inherit"
+                            }
+                        />
+                    </ListItemIcon>
+
+                    <ListItemText primary="Complain" />
+                </ListItemButton>
+            </React.Fragment>
+
+            <Divider sx={{ my: 1 }} />
+
+            <React.Fragment>
+                <ListSubheader
+                    component="div"
+                    inset
+                >
+                    User
+                </ListSubheader>
+
+                <ListItemButton
+                    component={Link}
+                    to="/Teacher/profile"
+                >
+                    <ListItemIcon>
+                        <AccountCircleOutlinedIcon
+                            color={
+                                location.pathname.startsWith(
+                                    "/Teacher/profile"
+                                )
+                                    ? "primary"
+                                    : "inherit"
+                            }
+                        />
+                    </ListItemIcon>
+
+                    <ListItemText primary="Profile" />
+                </ListItemButton>
+
+                <ListItemButton
+                    component={Link}
+                    to="/logout"
+                >
+                    <ListItemIcon>
+                        <ExitToAppIcon
+                            color={
+                                location.pathname.startsWith(
+                                    "/logout"
+                                )
+                                    ? "primary"
+                                    : "inherit"
+                            }
+                        />
+                    </ListItemIcon>
+
+                    <ListItemText primary="Logout" />
+                </ListItemButton>
+            </React.Fragment>
+        </>
+    );
+};
+
+export default TeacherSideBar;
