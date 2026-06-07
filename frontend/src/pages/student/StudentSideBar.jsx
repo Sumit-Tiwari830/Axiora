be99093e515ef3ccdda86a91241a9c772c3b7d22
Sumit-Tiwari830 <kiransumit2231@gmail.com>
2026-06-07 18:26:15 +0000

    feat: Add StudentSideBar component for student navigation
    Signed-off-by: Sumit-Tiwari830 <kiransumit2231@gmail.com>

---
+++ frontend/src/pages/student/StudentSideBar.jsx
@@ -0,0 +1,163 @@
+import React from "react";
+import {
+    Divider,
+    ListItemButton,
+    ListItemIcon,
+    ListItemText,
+    ListSubheader,
+} from "@mui/material";
+
+import { Link, useLocation } from "react-router-dom";
+
+import HomeIcon from "@mui/icons-material/Home";
+import ExitToAppIcon from "@mui/icons-material/ExitToApp";
+import AccountCircleOutlinedIcon from "@mui/icons-material/AccountCircleOutlined";
+import AnnouncementOutlinedIcon from "@mui/icons-material/AnnouncementOutlined";
+import ClassOutlinedIcon from "@mui/icons-material/ClassOutlined";
+import AssignmentIcon from "@mui/icons-material/Assignment";
+
+const StudentSideBar = () => {
+    const location = useLocation();
+
+    const isHome =
+        location.pathname === "/" ||
+        location.pathname === "/Student/dashboard";
+
+    return (
+        <>
+            {/* Main Navigation */}
+            <ListItemButton
+                component={Link}
+                to="/Student/dashboard"
+                selected={isHome}
+            >
+                <ListItemIcon>
+                    <HomeIcon
+                        color={isHome ? "primary" : "inherit"}
+                    />
+                </ListItemIcon>
+
+                <ListItemText primary="Dashboard" />
+            </ListItemButton>
+
+            <ListItemButton
+                component={Link}
+                to="/Student/subjects"
+                selected={location.pathname.startsWith(
+                    "/Student/subjects"
+                )}
+            >
+                <ListItemIcon>
+                    <AssignmentIcon
+                        color={
+                            location.pathname.startsWith(
+                                "/Student/subjects"
+                            )
+                                ? "primary"
+                                : "inherit"
+                        }
+                    />
+                </ListItemIcon>
+
+                <ListItemText primary="Subjects" />
+            </ListItemButton>
+
+            <ListItemButton
+                component={Link}
+                to="/Student/attendance"
+                selected={location.pathname.startsWith(
+                    "/Student/attendance"
+                )}
+            >
+                <ListItemIcon>
+                    <ClassOutlinedIcon
+                        color={
+                            location.pathname.startsWith(
+                                "/Student/attendance"
+                            )
+                                ? "primary"
+                                : "inherit"
+                        }
+                    />
+                </ListItemIcon>
+
+                <ListItemText primary="Attendance" />
+            </ListItemButton>
+
+            <ListItemButton
+                component={Link}
+                to="/Student/complain"
+                selected={location.pathname.startsWith(
+                    "/Student/complain"
+                )}
+            >
+                <ListItemIcon>
+                    <AnnouncementOutlinedIcon
+                        color={
+                            location.pathname.startsWith(
+                                "/Student/complain"
+                            )
+                                ? "primary"
+                                : "inherit"
+                        }
+                    />
+                </ListItemIcon>
+
+                <ListItemText primary="Complaints" />
+            </ListItemButton>
+
+            <Divider sx={{ my: 1 }} />
+
+            {/* User Section */}
+            <ListSubheader component="div" inset>
+                User
+            </ListSubheader>
+
+            <ListItemButton
+                component={Link}
+                to="/Student/profile"
+                selected={location.pathname.startsWith(
+                    "/Student/profile"
+                )}
+            >
+                <ListItemIcon>
+                    <AccountCircleOutlinedIcon
+                        color={
+                            location.pathname.startsWith(
+                                "/Student/profile"
+                            )
+                                ? "primary"
+                                : "inherit"
+                        }
+                    />
+                </ListItemIcon>
+
+                <ListItemText primary="Profile" />
+            </ListItemButton>
+
+            <ListItemButton
+                component={Link}
+                to="/logout"
+                selected={location.pathname.startsWith(
+                    "/logout"
+                )}
+            >
+                <ListItemIcon>
+                    <ExitToAppIcon
+                        color={
+                            location.pathname.startsWith(
+                                "/logout"
+                            )
+                                ? "primary"
+                                : "inherit"
+                        }
+                    />
+                </ListItemIcon>
+
+                <ListItemText primary="Logout" />
+            </ListItemButton>
+        </>
+    );
+};
+
+export default StudentSideBar;
